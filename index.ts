8515d165ae78376bc30a41938a99432d326e73bb
Krekinha <krekmg@gmail.com>
2024-07-15 13:01:30 -0300

    13:01

--- index.ts
+++ index.ts
@@ -1,35 +1,41 @@
-import { PrismaClient } from "@prisma/client";
+//import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
-import { z } from "zod";
+//import { z } from "zod";
 
 const app = fastify();
 
-const prisma = new PrismaClient();
+//const prisma = new PrismaClient();
+
+// app.get("/users", async () => {
+//   const users = await prisma.user.findMany();
+//   return { users };
+// });
+
+// app.post("/users", async (request, reply) => {
+//   const createUserSchema = z.object({
+//     nome: z.string(),
+//     email: z.string().email(),
+//   });
+
+//   // o zod valida os dados recebidos e não deixa o código prosseguir
+//   // sem que a validação tenha sido satisfeita
+//   const { nome, email } = createUserSchema.parse(request.body);
+
+//   await prisma.user.create({
+//     data: {
+//       nome,
+//       email,
+//     },
+//   });
+
+//   return reply.status(201).send();
+// });
+
+
 
 app.get("/users", async () => {
-  const users = await prisma.user.findMany();
-  return { users };
-});
-
-app.post("/users", async (request, reply) => {
-  const createUserSchema = z.object({
-    nome: z.string(),
-    email: z.string().email(),
+    return "olá mundo";
   });
-
-  // o zod valida os dados recebidos e não deixa o código prosseguir
-  // sem que a validação tenha sido satisfeita
-  const { nome, email } = createUserSchema.parse(request.body);
-
-  await prisma.user.create({
-    data: {
-      nome,
-      email,
-    },
-  });
-
-  return reply.status(201).send();
-});
 
 app
   .listen({
@@ -40,4 +46,4 @@ app
     console.log("HTTP Server Running");
   });
 
-  module.exports = app;
+module.exports = app;
